35d99f1017c94f1a587b18396c373121fa2cd91f
evnglbnrg <140699714+evnglbnrg@users.noreply.github.com>
2023-11-02 13:32:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let nbtn = document.getElementById('send');
+
+nbtn.addEventListener('click', showmsg);
+function showmsg()
+{ 
+    alert("Thank You For Messaging. We Will Get Back To You Soon");
+}
